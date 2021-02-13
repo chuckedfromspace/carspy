0abdcde1f8590b175e6a5e47f35193c8e9bee398
chuckedfromspace <43315257+chuckedfromspace@users.noreply.github.com>
2021-02-13 15:53:54 +0100

    Add project description

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 """The setup script."""
 from setuptools import setup, find_packages
 
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
 requirements = ['numpy']
 
 setup_requirements = []
@@ -29,7 +35,7 @@ setup(
                  "(CARS) in Python"),
     install_requires=requirements,
     license="BSD license",
-    # long_description=readme + '\n\n' + history,
+    long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='carspy',
     name='carspy',
