c65ab9625c0809849a21a94d6aa4604f8fba783a
chuckedfromspace <43315257+chuckedfromspace@users.noreply.github.com>
2021-07-05 09:14:25 +0200

    Bump version: 0.5.0 → 0.6.0

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/chuckedfromspace/carspy',
-    version='0.5.0',
+    version='0.6.0',
     zip_safe=False,
 )
